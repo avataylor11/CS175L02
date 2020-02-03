32dd50ecaeacac8dde5e1c83dec0ff19f422631a
Ava Taylor <s1270111@monmouth.edu>
2020-02-03 15:01:24 -0500

    House Project

--- CS175L/src/january/HouseProject.java
+++ CS175L/src/january/HouseProject.java
@@ -9,11 +9,12 @@ public class HouseProject {
 		
 		System.out.println("To find out the total cost of painting your house, enter the following criteria rounded to the nearest whole number.");
 		
-		System.out.println("Enter the width of the house :: ");
-		int houseWidth = keyboard.nextInt();
 		
 		System.out.println("Enter the length of the house :: ");
 		int houseLength = keyboard.nextInt();
+		
+		System.out.println("Enter the width of the house :: ");
+		int houseWidth = keyboard.nextInt();
 		
 		System.out.println("Enter the height of the house :: ");
 		int houseHeight = keyboard.nextInt();
@@ -21,23 +22,33 @@ public class HouseProject {
 		System.out.println("Enter the number of windows in the house :: ");
 		int numWindows = keyboard.nextInt();
 		
-		System.out.println("Enter the area of the windows in house :: ");
-		int dimWindows = keyboard.nextInt();
+		System.out.println("Enter the length of the windows in house :: ");
+		int windowLength = keyboard.nextInt();
+		
+		System.out.println("Enter the width of the windows in house :: ");
+		int windowWidth = keyboard.nextInt();
 		
 		System.out.println("Enter the number of doors in the house :: ");
 		int numDoors = keyboard.nextInt();
 		
-		System.out.println("Enter the dimensions of the doors in the house :: ");
-		int dimDoors = keyboard.nextInt ();
+		System.out.println("Enter the length of the doors in the house :: ");
+		int doorLength = keyboard.nextInt ();
+		
+		System.out.println("Enter the width of the doors in the house :: ");
+		int doorWidth = keyboard.nextInt ();
 		
 		System.out.println("Enter the charge per square foot to paint the house :: ");
 		int chargeFoot = keyboard.nextInt ();
 		
-		int peakTotal = ((houseLength * houseWidth) + ((houseLength * (houseHeight - houseWidth))/2));
+		int peakTotal = 2*(houseLength * houseWidth + (houseLength * (houseHeight - houseWidth))/2);
 
-		int normalTotal = houseLength * houseWidth;
+		int normalTotal = 2*(houseLength * houseWidth);
+		
+		int dimWindows = windowLength * windowWidth;
 		
 		int totalWindow = numWindows * dimWindows;
+		
+		int dimDoors = doorLength * doorWidth;
 				
 		int totalDoor = numDoors * dimDoors;
 		
